88328674a4d0a30c86d8187be8be9e0bd3858583
DatVM <datvm@outlook.com>
2016-03-30 01:58:46 +0700

    Finished the first version.

---
+++ FontCollection/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using FontCollection.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FontCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindowViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ViewModel = MainWindowViewModel.Load();
+
+            this.DataContext = this.ViewModel;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.ViewModel.Save();
+        }
+
+        private void lstFonts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selection = this.lstFonts.SelectedItem as FontFamily;
+
+            if (selection == null)
+            {
+                return;
+            }
+
+            this.lblPreview.FontFamily = selection;
+            this.ViewModel.SelectFont(selection);
+        }
+
+        private void mnuAddCollection_Click(object sender, RoutedEventArgs e)
+        {
+            var diagAdd = new AddCollection();
+            
+            if (diagAdd.ShowDialog() != true)
+            {
+                return;
+            }
+
+            try
+            {
+                this.ViewModel.AddCollection(diagAdd.CollectionName);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void mnuRemoveCollection_Click(object sender, RoutedEventArgs e)
+        {
+            var selection = this.cboCollections.SelectedItem as ViewModels.FontCollection;
+
+            if (selection == null || selection.Name == "ALL")
+            {
+                MessageBox.Show("Choose a collection to delete!", this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Are you sure to delete that collection?", this.Title, MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            this.ViewModel.DeleteCollection(selection);
+        }
+
+        private void cboCollections_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selection = this.cboCollections.SelectedItem as ViewModels.FontCollection;
+
+            this.ViewModel.FilterFontList(selection);
+        }
+
+        private void chkFontCollectionBinding_Checked(object sender, RoutedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+            var collectionAndFamilyName = chk.Tag as KeyValuePair<string, string>?;
+
+            if (collectionAndFamilyName == null)
+            {
+                return;
+            }
+
+            this.ViewModel.BindFontAndCollection(collectionAndFamilyName.Value.Value, collectionAndFamilyName.Value.Key, chk.IsChecked == true);
+        }
+    }
+}
---
+++ FontCollection/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FontCollection.ViewModels
+{
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private const string FilePath = "data.json";
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        [JsonIgnore]
+        public IEnumerable<FontFamily> InstalledFonts { get; private set; }
+
+        public List<FontCollection> FontCollections { get; set; } = new List<FontCollection>();
+
+        [JsonIgnore]
+        public IEnumerable<FontFamily> FilteredFontList { get; private set; }
+
+        [JsonIgnore]
+        public List<FontCollection> FontCollectionsUI
+        {
+            get
+            {
+                return new List<FontCollection>()
+                {
+                    new FontCollection() { Name = "ALL", },
+                }.Concat(this.FontCollections)
+                .ToList();
+            }
+        }
+
+        [JsonIgnore]
+        public List<FontCollectionBinding> CurrentFontBinding { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            var installedFont = new InstalledFontCollection();
+            this.InstalledFonts = installedFont.Families
+                .Select(q => new FontFamily(q.Name))
+                .ToList();
+
+            this.FilterFontList(null);
+        }
+
+        public void AddCollection(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new InvalidOperationException("Name must be entered");
+            }
+
+            foreach (var collection in this.FontCollectionsUI)
+            {
+                if (collection.Name.ToLower() == name.ToLower())
+                {
+                    throw new InvalidOperationException("Name already exist");
+                }
+            }
+
+            this.FontCollections.Add(new FontCollection()
+            {
+                Name = name,
+            });
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.FontCollections)));
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.FontCollectionsUI)));
+        }
+
+        public void DeleteCollection(FontCollection collection)
+        {
+            this.FontCollections.Remove(collection);
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.FontCollections)));
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.FontCollectionsUI)));
+        }
+
+        public void FilterFontList(FontCollection collection)
+        {
+            if (collection == null || collection.Name == "ALL")
+            {
+                this.FilteredFontList = this.InstalledFonts.ToList();
+            }
+            else
+            {
+                this.FilteredFontList = this.InstalledFonts
+                    .Where(q => collection.FontFamilies.Contains(q.Source))
+                    .ToList();
+            }
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.FilteredFontList)));
+        }
+
+        public void SelectFont(FontFamily fontFamily)
+        {
+            this.CurrentFontBinding = this.FontCollections.Select(q => new FontCollectionBinding()
+            {
+                CollectionName = q.Name,
+                Binded = q.FontFamilies.Contains(fontFamily.Source),
+                CollectionAndFamilyName = new KeyValuePair<string, string>(q.Name, fontFamily.FamilyNames.First().Value),
+            }).ToList();
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(nameof(this.CurrentFontBinding)));
+        }
+
+        public void BindFontAndCollection(string fontFamily, string collectionName, bool bind)
+        {
+            var collection = this.FontCollections.First(q => q.Name == collectionName);
+
+            if (bind)
+            {
+                collection.FontFamilies.Add(fontFamily);
+            }
+            else
+            {
+                collection.FontFamilies.Remove(fontFamily);
+            }
+        }
+
+        public static MainWindowViewModel Load()
+        {
+            if (File.Exists(FilePath))
+            {
+                return JsonConvert.DeserializeObject<MainWindowViewModel>(
+                    File.ReadAllText(FilePath, Encoding.UTF8));
+            }
+            else
+            {
+                return new MainWindowViewModel();
+            }
+        }
+
+        public void Save()
+        {
+            File.WriteAllText(FilePath, JsonConvert.SerializeObject(this), Encoding.UTF8);
+        }
+
+    }
+
+    public class FontCollection
+    {
+        public string Name { get; set; }
+        public HashSet<string> FontFamilies { get; set; } = new HashSet<string>();
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+    }
+
+    public class FontCollectionBinding
+    {
+        public string CollectionName { get; set; }
+        public bool Binded { get; set; }
+        public KeyValuePair<string, string> CollectionAndFamilyName { get; set; }
+    }
+
+}
